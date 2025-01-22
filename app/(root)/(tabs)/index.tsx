4ba870ee8c53f05956e664bb02ac9123281245a1
IjonK007 <kushta.joni@gmail.com>
2025-01-22 19:15:39 +0100

    home page top + featured card design

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,36 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import icons from "@/app/constants/icons";
+import images from "@/app/constants/images";
+import { Card, FeaturedCard } from "@/components/cards";
+import Search from "@/components/search";
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-blue-500 text-2xl font-bold font-rubik">Welcome to Realstate!</Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="../properties/1">Properties</Link>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full"/>
+            <View className="flex flex-col items-start justify-center ml-2"> 
+              <Text className="text-sm font-rubik text-black-100">Welcome back</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Ijon</Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <FeaturedCard />
+        <Card />
+      </View>
+    </SafeAreaView>
   );
 }
